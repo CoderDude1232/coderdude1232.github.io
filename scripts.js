c558f4e47a802ba6623b787c92b2fc8eca3fcea0
CoderDude1232 <100014515+CoderDude1232@users.noreply.github.com>
2023-03-18 06:55:36 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,46 +1,21 @@
-// Get references to DOM elements
-const browseButton = document.querySelector('nav a[href="#browse"]');
-const collectionsButton = document.querySelector('nav a[href="#collections"]');
-const main = document.querySelector('main');
-
-// Show some sample content when the "Browse" button is clicked
-browseButton.addEventListener('click', () => {
-  const section = document.createElement('section');
-  section.innerHTML = `
-    <h2>Browse Recipes</h2>
-    <ul>
-      <li><a href="#">Appetizers</a></li>
-      <li><a href="#">Main Courses</a></li>
-      <li><a href="#">Desserts</a></li>
-      <li><a href="#">Drinks</a></li>
-    </ul>
-  `;
-  main.innerHTML = '';
-  main.appendChild(section);
-});
-
-// Show some sample content when the "Collections" button is clicked
-collectionsButton.addEventListener('click', () => {
-  const section = document.createElement('section');
-  section.innerHTML = `
-    <h2>Collections</h2>
-    <ul>
-      <li><a href="#">Healthy Recipes</a></li>
-      <li><a href="#">Quick and Easy</a></li>
-      <li><a href="#">Family Favorites</a></li>
-      <li><a href="#">Seasonal Specialties</a></li>
-    </ul>
-  `;
-  main.innerHTML = '';
-  main.appendChild(section);
-});
-
 // Define a function to create a new recipe element
 function createRecipe() {
-  const featuredRecipes = document.querySelector(".featured-recipes");
-  const newRecipe = document.createElement("li");
-  newRecipe.innerHTML = "<img src='https://via.placeholder.com/300x200' alt='Recipe Image'><h3>New Recipe Title</h3>";
-  featuredRecipes.querySelector("ul").appendChild(newRecipe);
+  // Get the recipe details from the user
+  const recipeName = prompt("Enter the name of the recipe:");
+  const recipeDescription = prompt("Enter a brief description of the recipe:");
+  const recipeSection = prompt("Enter the section where the recipe should be added:");
+
+  // Find the section where the recipe should be added
+  const section = document.querySelector(`.${recipeSection}`);
+
+  // If the section exists, create a new recipe element and add it to the section
+  if (section) {
+    const newRecipe = document.createElement("li");
+    newRecipe.innerHTML = `<img src='https://via.placeholder.com/300x200' alt='Recipe Image'><h3>${recipeName}</h3><p>${recipeDescription}</p>`;
+    section.querySelector("ul").appendChild(newRecipe);
+  } else {
+    alert(`The section "${recipeSection}" does not exist.`);
+  }
 }
 
 // Define a function to create a new recipe category element
@@ -58,4 +33,3 @@ const button2 = document.getElementById("button2");
 // Add event listeners to each button
 button1.addEventListener("click", createRecipe);
 button2.addEventListener("click", createCategory);
-
